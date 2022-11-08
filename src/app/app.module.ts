0262e781bef225620b5056843dae6a9e1b0613fe
Emanuel Carrero <89891608+emanuelmcp@users.noreply.github.com>
2022-11-08 12:34:30 +0100

    Release

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {LOCALE_ID, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {VentasModule} from "./ventas/ventas.module";
+import {SharedModule} from "./shared/shared.module";
+import {AppRouterModule} from "./app-router.module";
+
+//Cambiar el locale de la app
+import localeEs from '@angular/common/locales/es'
+import {registerLocaleData} from "@angular/common";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+registerLocaleData(localeEs);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    VentasModule,
+    SharedModule,
+    AppRouterModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID, useValue: 'es'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
